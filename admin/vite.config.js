c586348947de4bebeada905f7ecd5611ed678735
Moinul Islam <moinulilm10@gmail.com>
2026-04-22 12:36:56 +0600

    refactor: migrate Tailwind CSS to Vite plugin and clean up CSS structure with layers and CSS variables

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,8 +6,8 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
   ],
 });
